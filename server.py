2341868e07e08e58161be41943e49d2423937b0f
Kalyan5252 <kalyanpendem007@gmail.com>
2025-08-03 22:04:33 +0530

    policy update

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # or ["*"] to allow all
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
